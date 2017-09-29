1d6b5272929f744bc8b9b256a287e6a97e908e66
Emily Morgan <emorgan05@gmail.com>
2017-09-29 14:51:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -60,4 +60,3 @@ song_2 = Song.create_by_name("Hello")
 song_3 = Song.create_by_name("Blank Space")
 song_4 = Song.create_by_name("Banana Pancake")
 song_5 = Song.create_by_name("Locked Out of Heaven")
-Song.alphabetical
