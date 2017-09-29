c07ec027c276a57f5c3a51e9c7b614275069b5d2
Emily Morgan <emorgan05@gmail.com>
2017-09-29 14:53:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,10 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.detect do |song|
-      song.name = name
+    @@all.each do |song|
+      if song.name == name
+        return song
+      end
     end
   end
 
