dd177a1cd315a958bd2accd502131912feb0023b
Emily Morgan <emorgan05@gmail.com>
2017-09-29 14:00:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,8 +38,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    self.find_by_name(name)
-    if self.find_by_name(name) == nil
+    if self.find_by_name(name)
+    else
       self.create_by_name(name)
     end
   end
